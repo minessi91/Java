4f7fb4ccf16113bf76d3e4e454fe9cdc5b1c3cb5
minessi91 <minessilva91@gmail.com>
2017-11-28 19:31:25 -0200

    Exemplo de métodos

---
+++ TesteFunc/src/Funcao.java
@@ -0,0 +1,36 @@
+
+public class Funcao {
+	
+	
+/*	static void soma(int a, int b) {
+		
+	int s = a + b;
+	System.out.println(" A soma é "+s);
+	
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Inicio do código");
+		soma(4,3);
+		System.out.println("Fim do código");
+	}*/
+	
+
+	static int soma(int a, int b) {
+		int s = a + b;
+		return s;
+	}
+		
+	public static void main(String[] args) {
+		
+		System.out.println("Inicio do código");
+		int sm = soma(4,3);
+		System.out.println("A soma é "+sm);
+		System.out.println("Fim do código");
+		
+	}
+		
+	
+}
+
